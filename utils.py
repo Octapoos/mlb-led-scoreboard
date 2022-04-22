a0ee879d0fc1ba28c19c9ee839c4d6ef6e3d4448
Octapoos <andpoos@gmail.com>
2022-04-21 21:09:22 -0400

    Update master with customised Config.json

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ except ImportError:
     from RGBMatrixEmulator import RGBMatrixOptions
 
 import argparse
-import collections
+import collections.abc
 
 import debug
 
@@ -180,7 +180,7 @@ def deep_update(source, overrides):
     Modify ``source`` in place.
     """
     for key, value in list(overrides.items()):
-        if isinstance(value, collections.Mapping) and value:
+        if isinstance(value, collections.abc.Mapping) and value:
             returned = deep_update(source.get(key, {}), value)
             source[key] = returned
         else:
